c225e15b5a9b63de1786efb0c09c1127119ba295
mich2k <inbox@gmichele.com>
2024-06-26 19:59:56 +0200

    + support for lower res images + support for lowres fs

--- create_ds/create_ds.py
+++ create_ds/create_ds.py
@@ -4,6 +4,8 @@ from PIL import Image
 from io import BytesIO
 import libsql_experimental as libsql
 from dotenv import load_dotenv
+import glob
+
 
 def image_to_base64(image_path):
     with open(image_path, "rb") as img_file:
@@ -46,7 +48,7 @@ def process_compression(img_path, mask_path, cmap_path):
     return img_buffered, mask_buffered, cmap_buffered
 
 
-def process_images(images_folder, cmaps_folder, db_name, table_name, AUTH_TOKEN, DB_URL, compress = True, max_images=None):
+def process_images(images_folder, cmaps_folder, db_name, table_name, AUTH_TOKEN, DB_URL, use_presplit_ds, compress, max_images=None):
     conn = libsql.connect(db_name, sync_url=DB_URL, auth_token=AUTH_TOKEN)    
     conn.sync()
 
@@ -60,19 +62,29 @@ def process_images(images_folder, cmaps_folder, db_name, table_name, AUTH_TOKEN,
     )
     ''')
     
-    cur=conn.cursor()
+    cur = conn.cursor()
 
-    try:
-        image_files = sorted([f for f in os.listdir(images_folder) if f.endswith('.jpg')])
-        mask_files = sorted([f for f in os.listdir(images_folder) if f.endswith('_mask.png')])
-        cmap_files = sorted([f for f in os.listdir(cmaps_folder) if f.endswith('.png') and f.startswith('cmapped_mask_')])
-    except FileNotFoundError:
+    if not use_presplit_ds:
+        try:
+            image_files = sorted([f for f in os.listdir(images_folder) if f.endswith('.jpg')])
+            mask_files = sorted([f for f in os.listdir(images_folder) if f.endswith('_mask.png')])
+            cmap_files = sorted([f for f in os.listdir(cmaps_folder) if f.endswith('.png') and f.startswith('cmapped_mask_')])
+        except FileNotFoundError:
+            images_folder = os.path.join('create_ds', images_folder)
+            cmaps_folder = os.path.join('create_ds', cmaps_folder)
+            image_files = sorted([f for f in os.listdir(images_folder) if f.endswith('.jpg')])
+            mask_files = sorted([f for f in os.listdir(images_folder) if f.endswith('_mask.png')])
+            cmap_files = sorted([f for f in os.listdir(cmaps_folder) if f.endswith('.png') and f.startswith('cmapped_mask_')])
+    else:
         images_folder = os.path.join('create_ds', images_folder)
-        cmaps_folder = os.path.join('create_ds', cmaps_folder)
-        image_files = sorted([f for f in os.listdir(images_folder) if f.endswith('.jpg')])
-        mask_files = sorted([f for f in os.listdir(images_folder) if f.endswith('_mask.png')])
-        cmap_files = sorted([f for f in os.listdir(cmaps_folder) if f.endswith('.png') and f.startswith('cmapped_mask_')])
+        image_files = sorted(glob.glob(os.path.join(images_folder, '**/sub_image_*.png')))
+        mask_files = sorted(glob.glob(os.path.join(images_folder, '**/sub_mask_*.png')))
+        cmap_files = sorted(glob.glob(os.path.join(images_folder, '**/sub_cmapped_mask_*.png'))) 
         
+    assert len(image_files) > 0, "No images found in the specified folder."
+    assert len(mask_files) > 0, "No masks found in the specified folder."
+    assert len(cmap_files) > 0, "No colormaps found in the specified folder."
+    
     if not (len(image_files) == len(mask_files) == len(cmap_files)):
         print("Mismatch in the number of image, mask, and colormap files.")
         return
@@ -91,25 +103,31 @@ def process_images(images_folder, cmaps_folder, db_name, table_name, AUTH_TOKEN,
     for idx, (img_file, mask_file, cmap_file) in enumerate(zip(image_files, mask_files, cmap_files)):
         if idx >= total_images:
             break
-
-        img_id = img_file.split('.')[0][6:]
-
-
-        if not (mask_file.startswith(f'sample{img_id}_mask') and cmap_file.startswith(f'cmapped_mask_{img_id}')):
-            print(f"ID mismatch found: {img_file}, {mask_file}, {cmap_file}")
-            continue
-
-
-        img_path = os.path.join(images_folder, img_file)
-        mask_path = os.path.join(images_folder, mask_file)
-        cmap_path = os.path.join(cmaps_folder, cmap_file)
         
-        if compress:
+        if use_presplit_ds:
+            img_id = img_file.split('/')[-1].split('_')[-1].split('.')[0]
+            if not img_id in mask_file and not img_id in cmap_file:
+                print(f"ID mismatch found: {img_file}, {mask_file}, {cmap_file}")
+                continue
+        else:
+            img_id = img_file.split('.')[0][6:]
+            if not (mask_file.startswith(f'sample{img_id}_mask') and cmap_file.startswith(f'cmapped_mask_{img_id}')):
+                print(f"ID mismatch found: {img_file}, {mask_file}, {cmap_file}")
+                continue
+            img_path = os.path.join(images_folder, img_file)
+            mask_path = os.path.join(images_folder, mask_file)
+            cmap_path = os.path.join(cmaps_folder, cmap_file)
+        
+        if compress and not use_presplit_ds: # can be adjusted for presplit ds
             img_b, mask_b, cmap_b = process_compression(img_path, mask_path, cmap_path)
             img_base64 = base64.b64encode(img_b.getvalue()).decode('ascii')
             mask_base64 = base64.b64encode(mask_b.getvalue()).decode('ascii')
             cmap_base64 = base64.b64encode(cmap_b.getvalue()).decode('ascii')
         else:
+            if use_presplit_ds:
+                img_path = img_file
+                mask_path = mask_file
+                cmap_path = cmap_file
             img_base64 = image_to_base64(img_path)
             mask_base64 = image_to_base64(mask_path)
             cmap_base64 = image_to_base64(cmap_path)
@@ -133,6 +151,13 @@ images_folder = "dataset"
 cmaps_folder = "whole_cmaps"
 db_name = "app.db"
 table_name = "images_dataset"
-max_images = 25
+max_images = 200
 
-process_images(images_folder, cmaps_folder, db_name, table_name, AUTH_TOKEN, DB_URL, compress, max_images )
+use_presplit_ds = True
+
+if use_presplit_ds:
+    images_folder += "_presplit"
+    table_name = 'low_' + table_name
+    compress = False
+
+process_images(images_folder, cmaps_folder, db_name, table_name, AUTH_TOKEN, DB_URL, use_presplit_ds, compress, max_images )
